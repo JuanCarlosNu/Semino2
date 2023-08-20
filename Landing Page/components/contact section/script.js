0ea5646c2cfe902fc27a41da2319030f291688ca
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-08-20 16:41:23 -0300

    created new sections

---
+++ Landing Page/components/contact section/script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // You can add code here to handle form submission, such as sending an email or saving the data.
+    // For now, this example simply prevents the default form submission behavior.
+});
